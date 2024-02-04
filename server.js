0c70f53b268f84651261e01f3cc502c3d04a28a3
Khiyanat-Deori <65724531+Khiyanat-Deori@users.noreply.github.com>
2024-02-04 11:44:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
-import path from 'path'
-import {fileURLToPath} from'url'
+// import path from 'path'
+// import {fileURLToPath} from'url'
 
 dotenv.config();
 connectDB();
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+// const __filename=fileURLToPath(import.meta.url);
+// const __dirname=path.dirname(__filename);
 const app=express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname,'./client/build')));
+// app.use(express.static(path.join(__dirname,'./client/build')));
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
@@ -26,9 +26,9 @@ app.use('/api/v1/product',productRoutes);
 
 
 
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
+// app.use('*',function(req,res){
+//     res.sendFile(path.join(__dirname,"./client/build/index.html"))
+// })
 
 const PORT= process.env.PORT || 8080;
 
